3c808e05961c85d276aebc59e8910c97b5811dc7
Per Lilja <perlilja@gmail.com>
2020-08-31 21:19:44 +0200

    Inital files

---
+++ Main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <SDL.h>
+
+
+void initializeWindow() {
+
+}
+
+void setup() {
+
+}
+
+void processInput() {
+
+}
+
+void update() {
+
+}
+
+void render() {
+
+}
+
+int main(int argc, char* args[]) {
+	initializeWindow();
+
+	setup();
+
+	while (1) {
+		processInput();
+		update();
+		render();
+	}
+	
+	return 0;
+}
